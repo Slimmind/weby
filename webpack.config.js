cea8d04cba574e9eedfc0c60034344ddd30ceb54
paul.s <paul.s@bigdropinc.com>
2020-02-24 17:14:29 +0200

    added webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,92 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-	entry: './src/javascript/index.js',
-	
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
+    entry: './src/js/index.js',
+    
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js', 
+        // publicPath: '/dist'
+    },
 
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			},
-			{
-				test: /\.(sa|sc|c)ss$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader
-					},
-					{
-						loader: 'css-loader',
-					},
-					{
-						loader: 'postcss-loader'
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							implementation: require('sass')
-						}
-					}
-				]
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							outputPath: 'images'
-						}
-					}
-				]
-			},
-			{
-				test: /\.(woff|woff2|ttf|otf|eot)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							outputPath: 'fonts'
-						}
-					}
-				]
-			}
-		]
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: 'bundle.css'
-		})
-	],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass')
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts'
+                        }
+                    }
+                ]
+            },
+            // {
+            //     test: /\.html$/,
+            //     use: ['html-loader']
+            // }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: 'src/index.html',
+            filename: 'index.html'
+        })
+    ],
 
-	mode: 'development'
+    mode: 'development'
 }
