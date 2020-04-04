9f1638d4dcf563d7d92f64e60e81dd6b7d30e376
slimmind <slimmind@gmail.com>
2020-04-04 17:23:24 +0300

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); 
 const isDev = process.env.NODE_ENV === 'development';
 
 const optimize = () => {
@@ -41,10 +42,25 @@ module.exports = {
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: fileName('css')
-        })
+        }),
+        new CopyWebpackPlugin([
+            { from: 'img', to: 'img' }
+        ]),
     ],
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                ]
+            },
             {
                 test: /\.(sa|sc|c)ss$/,
                 use: [
@@ -57,9 +73,6 @@ module.exports = {
                     },
                     {
                         loader: 'css-loader',
-                    },
-                    {
-                        loader: 'postcss-loader'
                     },
                     {
                         loader: 'sass-loader',
@@ -76,19 +89,6 @@ module.exports = {
                         loader: 'file-loader',
                         options: {
                             outputPath: 'fonts'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'img/',
-                            publicPath: 'img/'
                         }
                     }
                 ]
